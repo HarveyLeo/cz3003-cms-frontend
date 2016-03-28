222a67e8641ba492c17944a89d120c553106c6a1
Harvey <harveyleo.lhh@gmail.com>
2016-03-28 14:25:18 +0800

    Add submitted prompt & Fix timestamp bug

--- app/js/controllers/operator-controller.js
+++ app/js/controllers/operator-controller.js
@@ -1,9 +1,10 @@
 "use strict";
 
-cmsControllers.controller('createNewIncidentCtrl', ['$scope','IncidentCreationService',
-    function($scope, IncidentCreationService) {
+cmsControllers.controller('createNewIncidentCtrl', ['$scope','IncidentCreationService','$timeout',
+    function($scope, IncidentCreationService, $timeout) {
+
         $scope.incidentDetails = {
-            incident_timestamp: '1458380218',
+            incident_timestamp: '',
             incident_type: '',
             incident_address: '',
             incident_longitude: '',
@@ -12,22 +13,77 @@ cmsControllers.controller('createNewIncidentCtrl', ['$scope','IncidentCreationSe
             incident_contactNo: '',
             incident_description: '',
             incident_status:'',
-            agency:'',
+            agency:'NEA',
             operator: ''
         };
-        $scope.submitNewIncident = function(incidentDetails) {
-            console.log($scope.currentUser);
-            $scope.incidentDetails.operator = $scope.currentUser.user_id;
 
-            IncidentCreationService.submit(incidentDetails).then(function(data) {
+        $scope.convertToTimestamp = function() {
+            var time_str = $("#new_incident_time").val();
+
+            var parts = time_str.split(' ');
+            var time_parts = parts[0].split(':');
+
+            var hour = parseInt(time_parts[0]);
+            var minute = parseInt(time_parts[1]);
+
+            if (parts[1] == "PM"){
+                hour += 12;
+            }
+
+            var today = new Date();
+            var day = today.getDate();
+            var month = today.getMonth() + 1;
+            var year = today.getFullYear();
+
+            if(month < 10){
+                month = '0' + month;
+            }
+
+            if(day < 10){
+                day = '0' + day;
+            }
+
+            var result = year + '-' + month + '-' + day + ' ' + hour + ":" + minute + ':00';
+
+            $scope.incidentDetails.incident_timestamp = result;
+        };
+
+        $scope.submitNewIncident = function() {
+
+            $scope.incidentDetails.operator = $scope.currentUser.user_id;
+            $scope.incidentDetails.incident_latitude = $("#location").data("latitude");
+            $scope.incidentDetails.incident_longitude = $("#location").data("longitude");
+            $scope.convertToTimestamp();
+
+            IncidentCreationService.submit($scope.incidentDetails).then(function(data) {
                 console.log(data);
                 $scope.response = data;
-                $scope.showAlert();
+                $scope.showSuccess = true;
+                $timeout(function() {
+                    $scope.showSuccess = false;
+                    $("#new_incident_time").val("");
+                    $scope.incidentDetails = {
+                        incident_timestamp: '',
+                        incident_type: '',
+                        incident_address: '',
+                        incident_longitude: '',
+                        incident_latitude: '',
+                        incident_contactName: '',
+                        incident_contactNo: '',
+                        incident_description: '',
+                        incident_status: '',
+                        agency: 'NEA',
+                        operator: ''
+                    };
+                }, 2000);
             }, function() {
-                console.log("error");
                 $scope.response = "error";
             });
+
         };
+
+
+
     }
 ]);
 
